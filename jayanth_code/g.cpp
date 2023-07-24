724eac8a94dee2578781a776d1836459d7d659cd
pmjr2002 <jayanthrm.is21@rvce.edu.in>
2023-07-24 15:56:43 +0530

    first commit

---
+++ jayanth_code/g.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int b;
+    cin>>b;
+    cout<<n+b<<endl;
+}
